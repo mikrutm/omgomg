a2567c109a0c99ed2114aff31f75298430e68566
DaneAnaliza <87229869+mikrutm@users.noreply.github.com>
2023-10-03 14:20:39 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('WAO Raport')
+
+
+
+data = ""
+url = "google.pl"
+st.button("Click me")
+st.download_button("Download file", data)
+st.link_button("Go to gallery", url)
+#st.data_editor("Edit data", data)
+#st.checkbox("I agree")
+#st.toggle("Enable")
+#st.radio("Pick one", ["cats", "dogs"])
+#st.selectbox("Pick one", ["cats", "dogs"])
+#st.multiselect("Buy", ["milk", "apples", "potatoes"])
+#st.slider("Pick a number", 0, 100)
+#st.select_slider("Pick a size", ["S", "M", "L"])
+#st.text_input("First name")
+#st.number_input("Pick a number", 0, 10)
+#st.text_area("Text to translate")
+#st.date_input("Your birthday")
+#st.time_input("Meeting time")
+#st.file_uploader("Upload a CSV")
+#st.camera_input("Take a picture")
+#st.color_picker("Pick a color")
+

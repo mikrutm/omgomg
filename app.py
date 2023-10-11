22ca83bccc09ef426a13dc841a50de6903a5ab70
mikrutm <daneanalzialokalnie@gmail.com>
2023-10-11 13:03:36 +0200

    dsefer

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=Tru
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
     uploaded_file = df
     st.dataframe(df)
     today = date.today()

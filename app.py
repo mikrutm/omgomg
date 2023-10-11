88dc647fc56d624c8c6a21d080bf0b65cf39f8cc
mikrutm <daneanalzialokalnie@gmail.com>
2023-10-11 13:01:40 +0200

    sdfsd

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ import pandas as pd
 
 
 st.title('Twitter Tool')
-df = st.file_uploader("Upload a CSV")
-
-if df:
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
+    uploaded_file = df
     st.dataframe(df)
     today = date.today()
     st.text(today)

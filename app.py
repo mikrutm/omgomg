e9feff2bfa728bffe4f184d9a78e8b9932d42dbc
mikrutm <daneanalzialokalnie@gmail.com>
2023-10-11 12:53:49 +0200

    khgj

--- app.py
+++ app.py
@@ -7,37 +7,37 @@ import pandas as pd
 
 st.title('Twitter Tool')
 df = st.file_uploader("Upload a CSV")
-st.dataframe(df)
-st.text(str(df.columns))
+if df:
+    st.dataframe(df)
+    st.text(str(df.columns))
+    today = date.today()
+    st.text(today)
 
-today = date.today()
-st.text(today)
+    df['Date'] = pd.to_datetime(df['Date']).dt.date
+    end_date = today - timedelta(days = 1)
 
-df['Date'] = pd.to_datetime(df['Date']).dt.date
-end_date = today - timedelta(days = 1)
-
-start_date = today - timedelta(days =7 )
-mask = (df['Date'] >= start_date) & (df['Date'] <= end_date)  
-df = df[df.columns[:-1]]
-df = df.loc[mask]
-st.dataframe(df)
-
-
-# %%
-#end_date = today
-
-# Group by name and calculate the sum of numbers
-summary_df = df.groupby('Trend')['Inverted Position'].sum().reset_index()
+    start_date = today - timedelta(days =7 )
+    mask = (df['Date'] >= start_date) & (df['Date'] <= end_date)  
+    df = df[df.columns[:-1]]
+    df = df.loc[mask]
+    st.dataframe(df)
 
 
-# %%
-top=35
+    # %%
+    #end_date = today
 
-# Rename the columns
-summary_df.columns = ['Trend', 'PopIndex']
-df = summary_df.sort_values(by='PopIndex', ascending=   False).head(top).sort_values(by='PopIndex', ascending=   True)
-df=df.set_index("Trend")
-df["PopIndex"] = df["PopIndex"]/df["PopIndex"].max()
-st.dataframe(df)
+    # Group by name and calculate the sum of numbers
+    summary_df = df.groupby('Trend')['Inverted Position'].sum().reset_index()
+
+
+    # %%
+    top=35
+
+    # Rename the columns
+    summary_df.columns = ['Trend', 'PopIndex']
+    df = summary_df.sort_values(by='PopIndex', ascending=   False).head(top).sort_values(by='PopIndex', ascending=   True)
+    df=df.set_index("Trend")
+    df["PopIndex"] = df["PopIndex"]/df["PopIndex"].max()
+    st.dataframe(df)
 
 

c44e9c391948d770ccd0c6fe276d65e9a1b7fb4a
mikrutm <daneanalzialokalnie@gmail.com>
2023-11-03 10:11:18 +0100

    sdfs

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ import plotly
 import plotly.express as px 
 
 base="light"
+
 st.title('Twitter Tool WAO')
-
-
+tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+with tab1:
+    st.radio('Select one:', [1, 2])
 with st.sidebar:
   st.radio('Select one:', [1, 2])
 

3e1e44ee46f28e7a069ecbfe603fa574360faedc
mikrutm <daneanalzialokalnie@gmail.com>
2023-11-03 11:20:58 +0100

    sdfsdf

--- app.py
+++ app.py
@@ -95,6 +95,6 @@ with tab2:
     df_g = df_g['Inverted Position'].groupby(df_g['Date']).sum()
     st.dataframe(df_g)
     
-    fig_1 = px.bar(df_g, orientation='h',title=f"Popularność grupy tagów {txt} w okresie {start_date} - {end_date}",template="simple_white") 
+    fig_1 = px.bar(df_g,title=f"Popularność grupy tagów {txt} w okresie {start_date} - {end_date}",template="simple_white") 
     st.plotly_chart(fig_1, theme="streamlit")
 

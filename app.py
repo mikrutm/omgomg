de07d50a065c2f87d0fa189ec29d954519dcba39
mikrutm <daneanalzialokalnie@gmail.com>
2023-11-06 12:52:15 +0100

    adsf

--- app.py
+++ app.py
@@ -8,6 +8,27 @@ import plotly
 import plotly.express as px 
 
 base="light"
+def add_datetime_column(df):
+    def add_time_column(df):
+        times = []
+        hour = 0
+        interval = 50
+        for i in range(len(df)):
+            times.append(f"{str(hour).zfill(2)}:00")
+            if (i + 1) % interval == 0:
+                if hour == 0:
+                    hour = 23
+                else:
+                    hour = (hour - 1) % 24
+        df['Time'] = times
+        return df
+
+    df_with_time_column = add_time_column(df)
+
+    # Połączenie kolumn 'Date' i 'Time' w nową kolumnę 'DateTime' w formacie daty i czasu
+    df_with_time_column['DateTime'] = pd.to_datetime(df_with_time_column['Date'].astype(str) + ' ' + df_with_time_column['Time'])
+
+    return df_with_time_column
 
 
 tab1, tab2, tab3 = st.tabs(["Top", "Trend","Alert"])
@@ -59,6 +80,7 @@ with st.sidebar:
         )
         start_date = d[0]
         end_date = d[1]
+    df_time = add_datetime_column(df)
     
     df['Date'] = pd.to_datetime(df['Date']).dt.date
 
@@ -135,31 +157,8 @@ with tab2:
     fig_3 = px.bar(merged_df,x = "Date",y = ["Inverted Position_1","Inverted Position_2"],title=f"Porównanie popularności dwóch grup tagów w okresie {start_date} - {end_date}",template="simple_white") 
     st.plotly_chart(fig_3, theme="streamlit")
 with tab3:
-    def add_datetime_column(df):
-        def add_time_column(df):
-            times = []
-            hour = 0
-            interval = 50
-            for i in range(len(df)):
-                times.append(f"{str(hour).zfill(2)}:00")
-                if (i + 1) % interval == 0:
-                    if hour == 0:
-                        hour = 23
-                    else:
-                        hour = (hour - 1) % 24
-            df['Time'] = times
-            return df
-    
-        df_with_time_column = add_time_column(df)
-    
-        # Połączenie kolumn 'Date' i 'Time' w nową kolumnę 'DateTime' w formacie daty i czasu
-        df_with_time_column['DateTime'] = pd.to_datetime(df_with_time_column['Date'].astype(str) + ' ' + df_with_time_column['Time'])
-    
-        return df_with_time_column
 
 
     
     st.subheader('TBA')
-    st.dataframe(df)
-    df_time = add_datetime_column(df)
     st.dataframe(df_time)
